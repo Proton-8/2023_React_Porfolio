2d4763cf584ee6918517aad45eaf3d12dd140e4e
Ronald Shutter <r.a.shutter@gmail.com>
2023-03-08 16:23:59 -0800

    deployed -2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
-import React, { Fragment } from 'react';
+import "./App.css";
+import React, { Fragment } from "react";
 import { Navbar } from "./components/Navbar";
-import { About} from "./components/About";
-import { Project} from "./components/Project";
-import { Contact} from "./components/Contact";
-import { Footer} from "./components/Footer";
-import { Services } from './components/Services';
+import { About } from "./components/About";
+import { Project } from "./components/Project";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import { Services } from "./components/Services";
 
 function App() {
-
   return (
     <Fragment>
-      <div id="showcase">   
-   <Navbar/>
-   <About/>
+      <div id="showcase">
+        <Navbar />
+        <About />
       </div>
       <div id="showcase2">
-  <Services/>
-  </div>
-   <Project />
-    <br/>
-    <Contact />
-    <Footer />
-       </Fragment>
+        <Services />
+      </div>
+      <Project />
+      <br />
+      <Contact />
+      <Footer />
+    </Fragment>
   );
 }
 
